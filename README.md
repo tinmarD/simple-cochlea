89c79c2a3faff0aabf82522be1dcd4bf9b3fe681
tinmarD <martin.deudon@protonmail.com>
2018-10-10 14:58:38 +0200

    update README

---
+++ README.md
@@ -0,0 +1,16 @@
+# SimpleCochlea
+
+SimpleCochlea is a basic cochlea model for transforming an input sound into a spike-list.
+
+![](images/cochlea_schema.png)
+
+
+
+## Installation
+
+In a terminal run : 
+
+```shell
+pip install git+https://github.com/tinmarD/simple-cochlea.git@master
+```
+
--- examples/cochlea_creation.py
+++ examples/cochlea_creation.py
@@ -8,7 +8,7 @@ from simplecochlea import *
 matplotlib.use('TkAgg')
 
 
-fs, fmin, fmax, freq_scale, n_channels = 44100, 200, 8000, 'erbscale', 100
+fs, fmin, fmax, freq_scale, n_channels = 44100, 200, 8000, 'erbscale', 1000
 comp_factor, comp_gain = 0.3, 1.5
 tau, v_thresh, v_spike = np.linspace(0.001, 0.0004, n_channels), np.linspace(0.3, 0.17, n_channels), 0.5
 
@@ -18,5 +18,5 @@ cochlea_simp = Cochlea(n_channels, fs, fmin, fmax, freq_scale, comp_factor=comp_
 
 print(cochlea_simp)
 
-spikelist_sin = cochlea_simp.process_test_signal('sin', channel_pos=50)
+spikelist_sin = cochlea_simp.process_test_signal('sin', channel_pos=50, f_sin=400)
 spikelist_sin.plot()
